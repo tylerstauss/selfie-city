b5873160d2de5f5bd2ddb249ebf61d27b9e7f859
Tyler Stauss <tyler.e.stauss@gmail.com>
2014-06-30 08:41:05 -0700

    initial commit and setup of selfie-city

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SelfieCity::Application.config.secret_token = '7b5fa8ab421227641a1a0b300a1d8ea689c82f51869243bc161a8937d9b98327a3ee909d41ebcf98d62cbbc288f03428bd7fb61eab1748e01ecc698915a21d27'
