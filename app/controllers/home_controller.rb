a32f7c9c2d0d9dbb18522267ebad9502caf62db2
Tyler Stauss <tyler.e.stauss@gmail.com>
2014-06-30 16:03:33 -0700

    removing p from code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-  # 	client = Instagram::Client.new
-		# @results = client.get_posts
+
   end
 end
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,5 @@
 class PhotosController < ApplicationController
 	def index
-		client = Instagram::Client.new
-		@results = client.get_posts
 		render json: Photo.all
 	end
 
