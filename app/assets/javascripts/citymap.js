983e00f998bede995f9d933101c05a477610d777
Tyler Stauss <tyler.e.stauss@gmail.com>
2014-07-02 20:19:52 -0700

    fixed multiple markers on the city show page and also centered the map

--- app/assets/javascripts/citymap.js
+++ app/assets/javascripts/citymap.js
@@ -18,29 +18,41 @@ var CityMap = {
 		
 	},
 	addMarkers: function(data, map){
-		for (var i = 0; i < data.length; i++) {
+		var infowindow = new google.maps.InfoWindow();
+		var marker, i, contentString;
+		
+		for (i = 0; i < data.length; i++) {
 			var myLatlng = new google.maps.LatLng(data[i]['latitude'], data[i]['longitude']);
-			var marker = new google.maps.Marker({
+			marker = new google.maps.Marker({
 				position: myLatlng,
 				map: map,
 				title: data[i]['city_name']
-			})
-			var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<div id="bodyContent">'+
-      '<p><img src="'+data[i]["url"]+'"></p>'+
-      '<p>'+data[i]["instagram_text"]+'<a href="/photos/'+data[i]["id"]+'"> View Full Size Image</a>'+'	</p>'+
-      '</div>'+
-      '</div>';
-			var infowindow = new google.maps.InfoWindow({
-      	content: contentString
-  		});
+			});
+
+			
+
+			google.maps.event.addListener(marker, 'click', (function(marker, i, map) {
+        return function() {
+	        var contentString = '<div id="content">'+
+			      '<div id="siteNotice">'+
+			      '</div>'+
+			      '<div id="bodyContent">'+
+			      '<p><img src="'+data[i]["url"]+'"></p>'+
+			      '<p>'+data[i]["instagram_text"]+'<a href="/photos/'+data[i]["id"]+'"> View Full Size Image</a>'+'	</p>'+
+			      '</div>'+
+			      '</div>';
+          infowindow.setContent(contentString);
+          infowindow.open(map, marker);
+        }
+      })(marker, i, map));
+
+			
+		
+
+  		
 		};
 		
-		google.maps.event.addListener(marker, 'click', function() {
-    infowindow.open(map,marker);
-  });
+		
 	}
 
 }
